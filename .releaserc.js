ff8f8fb178cfff1a94b048c29e1bf898f049b607
Hobart2967 <hobart2967@outlook.com>
2024-08-14 00:35:35 +0200

    ci: Fix ci not pushing package.json update back to repo

--- .releaserc.js
+++ .releaserc.js
@@ -18,6 +18,9 @@ export default {
 		'@semantic-release/release-notes-generator',
 		'@semantic-release/github',
     '@semantic-release/npm',
-    '@semantic-release/git',
+    ["@semantic-release/git", {
+      "assets": ["package.json"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }]
 	],
 };
